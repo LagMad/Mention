6454e735546108f60bb2aa7491f7b9e6e6537efc
Jeremmy <hizkiajeremmy@gmail.com>
2024-06-05 20:42:48 +0700

    initial commit

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import Navbar from "../shared/Navbar";
+// import Footer from "../shared/Footer";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="bg-white pt-16 text-black font-Poppins">
+      {/* <Navbar /> */}
+      {children}
+      {/* <Footer/> */}
+    </div>
+  );
+};
+
+export default MainLayout;
